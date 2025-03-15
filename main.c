cb7b3c662b7fb07e9c74b01ee88e71d9e2314a89
Vlad50-50 <auditorrevizor@gmail.com>
2025-03-15 22:24:18 +0200

    c-explorer

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 
 void listFiles(const char *directory) {
     WIN32_FIND_DATA findFileData;
+    SYSTEMTIME sysTime;
     char path[MAX_PATH];
     snprintf(path, sizeof(path), "%s\\*", directory);
     HANDLE hFind = FindFirstFile(path, &findFileData);
@@ -14,13 +15,28 @@ void listFiles(const char *directory) {
         printf("Files in %s:\n", directory);
         do {
             const char *name = findFileData.cFileName;
+            FILETIME dateOfCreation = findFileData.ftCreationTime;
+            FileTimeToSystemTime(&findFileData.ftCreationTime, &sysTime);
+
             if(findFileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY){
                 if (strcmp(name, ".") != 0 && strcmp(name, "..") != 0){
-                    printf("[DIR] %s\\%s\n", directory, name);
+                    printf("[DIR]%s | %s | %02d/%02d/%04d %02d:%02d:%02d \n", directory, name,
+                        sysTime.wDay, 
+                        sysTime.wMonth, 
+                        sysTime.wYear,
+                        sysTime.wHour,
+                        sysTime.wMinute, 
+                        sysTime.wSecond);
                 }
             }
             else {
-                printf("    %s\\%s\n", directory, name);
+                printf("     %s | %s | %02d/%02d/%04d %02d:%02d:%02d \n", directory, name, 
+                    sysTime.wDay, 
+                    sysTime.wMonth, 
+                    sysTime.wYear,
+                    sysTime.wHour,
+                    sysTime.wMinute, 
+                    sysTime.wSecond);
             }
         } while (FindNextFile(hFind, &findFileData) != 0);
         FindClose(hFind);
@@ -28,6 +44,15 @@ void listFiles(const char *directory) {
 }
 
 int main(){
-    listFiles("./");
+    SetConsoleOutputCP(CP_UTF8);
+    const char addr[120];
+    while(1){
+        printf("Enter the addres of directory: (exemple : ../ , ./): ");
+        scanf("%s", &addr);
+        
+        listFiles(addr);
+    }
+    
+
     return 0;
 }
